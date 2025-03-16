f150abdfd6f2d460b12ee302b0a71811be629842
ManikanthDaru <158070196+ManikanthDaru@users.noreply.github.com>
2025-03-16 16:25:21 +0530

    Update claim.model.js

--- backend/models/claim.model.js
+++ backend/models/claim.model.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 
 const claimSchema = new mongoose.Schema({
     ip: {
-        type: String,
+        type: [String],
         required: true
     },
     sessionId: {
